144c033723f8de2444b19db696f7f0f0c6f44bcd
destolyar <dead.stolyar@gmail.com>
2022-05-03 22:58:57 +0300

    fech: theme switching added

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,8 +1,13 @@
 import { LayoutProps } from "../enities/interfaces/layout"
-
+import '../styles/components/layout.scss'
+import {useSelector} from 'react-redux'
 
 export const Layout = ({children}: LayoutProps) => {
+  const theme = useSelector((state: {theme: string}) => state.theme)
+  
   return(
-    <div className="layout">{children}</div>
+    <div className={(theme === 'Light') ? "layout" : "layout-dark"}>
+      {children}
+    </div>
   )
 }
--- src/app/components/ToDoList/ToDoListItem.tsx
+++ src/app/components/ToDoList/ToDoListItem.tsx
@@ -1,8 +1,10 @@
 import '../../styles/components/todo-item.scss'
+import { useSelector } from 'react-redux'
 
 export const ToDoListItem = () => {
+  let theme = useSelector((state: any) => state.theme)
   return(
-    <div className="todo-item">
+    <div className="todo-item" style={(theme !== 'Light') ? {backgroundColor: "#2e2e2e"} : {}}>
       <input className='todo-item__checkbox' type="checkbox" id='todo-item-1'/>
       <label className='todo-item__checkbox-label' htmlFor="todo-item-1"></label>
       <h3 className='todo-item__title'>Item</h3>
